63de67fce9c1a2fec5e69e2adf4735e042cffcf7
xiaoc <i@xiaoc.cn>
2021-12-20 10:29:18 +0800

    mongodb index

--- web_monitor/script/mongo_index.py
+++ web_monitor/script/mongo_index.py
@@ -1,21 +1,13 @@
 # -*- coding:utf-8 -*-
 import realpath
-from models.auto_down_anime import model
+from models.web_monitor import model
 
 
-_ = model.AnimeList()
-_.ensure_index([('media_id', -1)])
-_.ensure_index([('atime', -1)])
-_.ensure_index([('title', -1), ('atime', -1)])
-_.ensure_index([('desc', -1), ('atime', -1)])
-_.ensure_index([('media_id', -1), ('atime', -1)])
-_.ensure_index([('season_id', -1), ('atime', -1)])
+_ = model.WebLog()
+_.ensure_index([('id', -1), ('atime', -1)])
 
+_ = model.WebList()
+_.ensure_index([('enable', -1), ('atime', -1)])
 
-_ = model.EpisodeList()
-_.ensure_index([('id', -1)])
-_.ensure_index([('end', -1)])
-_.ensure_index([('atime', -1)])
-_.ensure_index([('season_id', -1), ('atime', -1)])
-_.ensure_index([('down_status', -1), ('atime', -1)])
-_.ensure_index([('long_title', -1), ('atime', -1)])
+_ = model.SSLList()
+_.ensure_index([('enable', -1), ('atime', -1)])
