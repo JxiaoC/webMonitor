6b0239ae39931ddee5e74742f53e92db7dd0a45c
xiaoc <i@xiaoc.cn>
2022-01-04 10:01:56 +0800

    ssl监控容错

--- web_monitor/task/ssl_monitor.py
+++ web_monitor/task/ssl_monitor.py
@@ -10,10 +10,13 @@ tb_ssl_list = model.SSLList()
 ssl_min_day = int(setting.get().get('ssl_min_day', '1'))
 
 for f in tb_ssl_list.find({'enable': True}):
-    rst_time = ssl_monitor.ref_ssl_time(f['_id'])
-    rst_day = int((rst_time - datetime.datetime.now()).total_seconds() / 86400)
-    print('rst_day', rst_day)
-    if rst_day <= ssl_min_day:
-        print('报警')
-        msg = '%s SSL证书还有%s天过期' % (f.get('host', ''), rst_day)
-        tools.send_server_jiang_msg(msg, msg)
+    try:
+        rst_time = ssl_monitor.ref_ssl_time(f['_id'])
+        rst_day = int((rst_time - datetime.datetime.now()).total_seconds() / 86400)
+        print('rst_day', rst_day)
+        if rst_day <= ssl_min_day:
+            print('报警')
+            msg = '%s SSL证书还有%s天过期' % (f.get('host', ''), rst_day)
+            tools.send_server_jiang_msg(msg, msg)
+    except Exception as e:
+        print(f['host'], e)
